2de4de725db1483d23bc4b80448437199d62d14c
Rustem <tukhvatullin.rustem@gmail.com>
2024-01-01 15:58:00 +0400

    1

--- ChallengeStarter/main.swift
+++ ChallengeStarter/main.swift
@@ -11,5 +11,66 @@ let resourceSets = ["example"]
 
 // MARK: - Direct Declaration
 // Invoke using direct declarations
-var day01 = SampleChallenge.Solution00(datasets: resourceSets, algorithms: [.part01, .part02])
-day01.execute()
+//var day01 = AdventOfCode2023.Day01(datasets: resourceSets, algorithms: [.part01])
+//day01.execute()
+
+//var day02 = AdventOfCode2023.Day02(datasets: resourceSets, algorithms: [.part01, .part02])
+//day02.execute()
+
+//var day03 = AdventOfCode2023.Day03(datasets: resourceSets, algorithms: [.part01, .part02])
+//day03.execute()
+
+//var day04 = AdventOfCode2023.Day04(datasets: resourceSets, algorithms: [.part01, .part02])
+//day04.execute()
+//
+//var day05 = AdventOfCode2023.Day05(datasets: resourceSets, algorithms: [.part01, .part02])
+//day05.execute()
+//
+//var day06 = AdventOfCode2023.Day06(datasets: resourceSets, algorithms: [.part01, .part02])
+//day06.execute()
+//
+//var day08 = AdventOfCode2023.Day08(datasets: resourceSets, algorithms: [.part01, .part02])
+//daн08.execute()
+//
+//var day09 = AdventOfCode2023.Day09(datasets: resourceSets, algorithms: [.part01, .part02])
+//day09.execute()
+//
+//var day10 = AdventOfCode2023.Day10(datasets: resourceSets, algorithms: [.part01, .part02])
+//day10.execute()
+//
+//var day11 = AdventOfCode2023.Day11(datasets: resourceSets, algorithms: [.part01, .part02])
+//day11.execute()
+//
+//var day12 = AdventOfCode2023.Day12(datasets: resourceSets, algorithms: [.part01, .part02])
+//day12.execute()
+//
+//var day13 = AdventOfCode2023.Day13(datasets: resourceSets, algorithms: [.part01, .part02])
+//day13.execute()
+//
+//var day14 = AdventOfCode2023.Day14(datasets: resourceSets, algorithms: [.part01, .part02])
+//day14.execute()
+//
+//var day16 = AdventOfCode2023.Day16(datasets: resourceSets, algorithms: [.part01, .part02])
+//day16.execute()
+//
+//var day18 = AdventOfCode2023.Day18(datasets: resourceSets, algorithms: [.part01, .part02])
+//day18.execute()
+//
+//var day19 = AdventOfCode2023.Day19(datasets: resourceSets, algorithms: [.part01, .part02])
+//day19.execute()
+//
+//var day20 = AdventOfCode2023.Day20(datasets: resourceSets, algorithms: [.part01, .part02])
+//day20.execute()
+//
+//var day21 = AdventOfCode2023.Day21(datasets: resourceSets, algorithms: [.part01, .part02])
+//day21.execute()
+//
+//var day22 = AdventOfCode2023.Day22(datasets: resourceSets, algorithms: [.part01, .part02])
+//day22.execute()
+//
+//var day23 = AdventOfCode2023.Day23(datasets: resourceSets, algorithms: [.part01, .part02])
+//day23.execute()
+
+var day24 = AdventOfCode2023.Day24(datasets: resourceSets, algorithms: [.part01, .part02])
+day24.execute()
+
